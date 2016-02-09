7747b8f4d65ed55cfd6dd575b62b160449c92722
Justin Terry <juterry@microsoft.com>
2016-02-09 15:59:43 -0800

    Initial commit of Docker PS project

---
+++ src/Docker.PowerShell/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Docker.PowerShell")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Docker.PowerShell")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
