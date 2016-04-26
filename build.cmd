d8fb5ac9b3c1c92bc3869e45c0c652344d43a1a2
Stefan J. Wernli <swernli@microsoft.com>
2016-04-25 18:46:03 -0700

    Adding vscode tasks and removing build.cmd
    Signed-off-by: Stefan J. Wernli <swernli@microsoft.com>

--- build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-IF /I "%1"=="-f" (del .\src\*project.lock.json /Q /S)
-
-dotnet restore
-dotnet build .\src\Docker.PowerShell -o .\artifacts\bin\Docker.PowerShell\Debug\net46 -f net46 -r win
