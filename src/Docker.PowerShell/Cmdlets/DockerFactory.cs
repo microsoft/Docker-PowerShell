a68a62642d137f009ceff8fd12d3ef7db71fc575
Stefan J. Wernli <swernli@microsoft.com>
2016-05-09 16:47:54 -0700

    Adding dependency on DOCKER_TLS_VERIFY
    Adding dependency on DOCKER_TLS_VERIFY environment variable to mimic behavior of docker CLI, ie: DOCKER_CERT_PATH will only be used if the DOCKER_TLS_VERIFY is also set.  Explicitly setting CertificateLocation parameter will still trigger TLS usage for ease of use regardless of environment variables.
    
    Resolves https://github.com/Microsoft/Docker-PowerShell/issues/52

--- src/Docker.PowerShell/Cmdlets/DockerFactory.cs
+++ src/Docker.PowerShell/Cmdlets/DockerFactory.cs
@@ -13,15 +13,14 @@ public class DockerFactory
     public static DockerClient CreateClient(string HostAddress, string CertificateLocation)
     {
         HostAddress = HostAddress ?? Environment.GetEnvironmentVariable("DOCKER_HOST") ?? "npipe://./pipe/docker_engine";
-        CertificateLocation = CertificateLocation ?? Environment.GetEnvironmentVariable("DOCKER_CERT_PATH");
-
+        
         CertificateCredentials cred = null;
-        if (!string.IsNullOrEmpty(CertificateLocation))
+        if (!string.IsNullOrEmpty(CertificateLocation) || Environment.GetEnvironmentVariable("DOCKER_TLS_VERIFY") == "1")
         {
             // Try to find a certificate for secure connections.
             cred = new CertificateCredentials(
                     new X509Certificate2(
-                        System.IO.Path.Combine(CertificateLocation, KeyFileName),
+                        System.IO.Path.Combine(CertificateLocation ?? Environment.GetEnvironmentVariable("DOCKER_CERT_PATH"), KeyFileName),
                         certPass));
 
             //BUGBUG(swernli) - Remove this later in favor of something better.
