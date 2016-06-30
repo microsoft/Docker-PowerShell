799dcd0ee218b6112b31ca58450a495fff836423
Stefan J. Wernli <swernli@microsoft.com>
2016-06-30 01:11:03 -0700

    Adding New/Remove-ContainerNet, updating Get.
    This adds functionality for New-ContainerNet, Remove-ContainerNet, and renames Get-ContainerNetwork to Get-ContainerNet to avoid collision with in-box cmdlets.  It also adds parameters for name and ID to Get, as well as an argument completer for network objects.

---
+++ src/Docker.PowerShell/Cmdlets/RemoveContainerNetwork.cs
@@ -0,0 +1,32 @@
+﻿using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace Docker.PowerShell.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Remove, "ContainerNet",
+            DefaultParameterSetName = CommonParameterSetNames.Default)]
+    public class RemoveContainerNet : NetworkOperationCmdlet
+    {
+        #region Parameters
+
+        /// <summary>
+        /// Whether or not to force the removal of the image.
+        /// </summary>
+        [Parameter]
+        public SwitchParameter Force { get; set; }
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task ProcessRecordAsync()
+        {
+            foreach (var id in ParameterResolvers.GetNetworkIds(Network, Id))
+            {
+                await DkrClient.Networks.DeleteNetworkAsync(id);
+            }
+        }
+
+        #endregion
+    }
+}
